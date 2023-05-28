92f5cfc62e69129b28866c4a864c4de957ac2c83
TAAT <20purpleblue10@gmail.com>
2023-05-28 11:07:56 +0900

    initial commit.

---
+++ TruncatedText/TruncatedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruncatedTextApp.swift
+//  TruncatedText
+//
+//  Created by TAAT on 2023/05/28.
+//
+
+import SwiftUI
+
+@main
+struct TruncatedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
